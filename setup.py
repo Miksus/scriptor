5645f35bf8534f7ae9eb917d3e5fdc445a1ea14b
Mikael Koli <koli.mikael@gmail.com>
2022-11-12 11:38:25 +0200

    add: setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import versioneer
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="scriptor",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Mikael Koli",
+    author_email="koli.mikael@gmail.com",
+    url="https://github.com/Miksus/scriptor.git",
+    package_data={package: ["py.typed", "*.pyi", "**/*.pyi"] for package in find_packages()},
+    packages=find_packages(),
+    zip_safe=False,
+    description="High-level command-line abstraction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Communications :: Email",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+
+        "Intended Audience :: Developers",
+    ],
+    include_package_data=True, # for MANIFEST.in
+    python_requires='>=3.8.0',
+
+    install_requires = [],
+)
