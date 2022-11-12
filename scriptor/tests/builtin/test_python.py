61fadad5a9f3150e46541d5c2968396e62482ada
Mikael Koli <koli.mikael@gmail.com>
2022-11-12 18:25:47 +0200

    test: restructured python tests

--- scriptor/tests/builtin/test_python.py
+++ scriptor/tests/builtin/test_python.py
@@ -5,7 +5,7 @@ from scriptor.builtin import current, python
 def test_current():
     assert current.program == (sys.executable,)
 
-def test_python(tmpdir):
+def test_python_run(tmpdir):
     tmpdir.mkdir("files")
     py_file = tmpdir.join("files/myscript.py")
     py_file.write(dedent("""
@@ -13,8 +13,10 @@ def test_python(tmpdir):
         """))
 
     assert current.run_code("print('Hello world')") == 'Hello world'
-    assert current.version.startswith("Python 3.")
-    assert current.full_version.startswith("Python 3.")
     assert current.run_script(py_file) == "Hello world"
     assert current.use(cwd=tmpdir).run_module("files.myscript") == "Hello world"
+
+def test_python_attrs():
+    assert current.version.startswith("Python 3.")
+    assert current.full_version.startswith("Python 3.")
     assert current.help.startswith("usage: python [option] ... [-c cmd | -m mod | file | -] [arg] ...")
